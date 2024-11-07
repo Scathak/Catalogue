8f8de2a4c465f4ba962b945f19e94ef8063b9cbb
Scathak <scathakocridera@gmail.com>
2024-11-07 20:45:43 +0300

    The catalog updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,20 @@
-import imageRef from "./assets/react-core-concepts.png";
+import imageRef from "./assets/Ori-banner-01.png";
 import { CORE_CONCEPTS } from "./data";
 
-
-const reactDescriptions = ['Fundamentals','Crucial','Core']; 
-
-function genRandomInt(max){
-  return Math.floor(Math.random()*(max+1));
-}
-
 function Header(){
-  const description = reactDescriptions[genRandomInt(2)];
+
   return (
     <header>
-    <img src={imageRef} alt="Stylized atom" />
-    <h1>React Essentials</h1>
-    <p>
-      {description} React concepts you will need for almost any app you are
-      going to build!
-    </p>
+    <img src={imageRef} alt="Banner Origami" />
+    <h1>Origami: Your Gateway to Creativity and Mindfulness</h1>
   </header>)
 }
-//function CoreConcept({image, title, description}){
-function CoreConcept({... priority}){
+function CoreConcept({image, title, description}){
   return(
     <li> 
-      <img src={priority.image} alt={priority.title}/>
-      <h3> {priority.title}</h3>
-      <p> {priority.description}</p>
+      <img src={image} alt={title}/>
+      <h3> {title}</h3>
+      <p> {description}</p>
     </li>
   )
 }
@@ -35,26 +23,31 @@ function App() {
     <div>
       <Header/>
       <main>
+        <div>
+          <p>
+            Welcome to our exclusive Origami Figures Catalog, a curated collection dedicated to the timeless art of paper folding. Origami, a Japanese art form with roots extending back over a thousand years, is much more than a pastime; it is a journey of creativity, precision, and patience that enhances focus, calms the mind, and brings joy to people of all ages. Each piece within our catalog is thoughtfully designed to inspire enthusiasts and beginners alike to explore the beauty and meditative qualities of origami.
+          </p><p>
+            Our catalog offers a diverse selection of step-by-step instructions for creating intricate animals, elegant flowers, geometric shapes, and much more. Whether you're looking to foster your creativity, improve your motor skills, or simply find a moment of peace, origami can provide all these benefits and more. Experience the pleasure of transforming a single sheet of paper into a stunning 3D figure — no additional tools required.
+          </p><p>
+            Download our catalog today and embark on a creative adventure that combines tradition with self-expression. Join us in celebrating origami, an art form that balances simplicity and complexity, offering a rewarding experience with every fold.
+          </p>
+        </div>
         <section id="core-concepts">
-          <h2>Core Concepts</h2>
+          <h2>Models</h2>
           <ul>
-            <CoreConcept 
-              title = {CORE_CONCEPTS[0].title}
-              description = {CORE_CONCEPTS[0].description}
-              image = {CORE_CONCEPTS[0].image}
-            />
-            <CoreConcept 
-              title = {CORE_CONCEPTS[1].title}
-              description = {CORE_CONCEPTS[1].description}
-              image = {CORE_CONCEPTS[1].image}
-            />
-            <CoreConcept 
-              title = {CORE_CONCEPTS[2].title}
-              description = {CORE_CONCEPTS[2].description}
-              image = {CORE_CONCEPTS[2].image}
-            />
-            <CoreConcept {... CORE_CONCEPTS[3]}
-            />
+            <CoreConcept {... CORE_CONCEPTS[0]} />
+            <CoreConcept {... CORE_CONCEPTS[1]} />
+            <CoreConcept {... CORE_CONCEPTS[2]} />
+            <CoreConcept {... CORE_CONCEPTS[3]} />
+            <CoreConcept {... CORE_CONCEPTS[4]} />
+            <CoreConcept {... CORE_CONCEPTS[5]} />
+            <CoreConcept {... CORE_CONCEPTS[6]} />
+            <CoreConcept {... CORE_CONCEPTS[7]} />
+            <CoreConcept {... CORE_CONCEPTS[8]} />
+            <CoreConcept {... CORE_CONCEPTS[9]} />
+            <CoreConcept {... CORE_CONCEPTS[10]} />
+            <CoreConcept {... CORE_CONCEPTS[11]} />
+            <CoreConcept {... CORE_CONCEPTS[12]} />
           </ul>
         </section>
       </main>
--- src/data.js
+++ src/data.js
@@ -1,31 +1,97 @@
-import componentsImg from './assets/components.png';
-import propsImg from './assets/config.png';
+import oriFlower1 from './assets/Ori-flower-01.png';
+import oriFlower2 from './assets/Ori-flower-03.png';
+import oriFlower3 from './assets/Ori-flower-04.png';
+import oriFlower4 from './assets/Ori-flower-05.png';
+import oriFlower5 from './assets/Ori-flower-06.png';
+import oriAnimal1 from './assets/Ori-animal-01.png';
+import oriAnimal2 from './assets/Ori-animal-02.png';
+import oriAnimal3 from './assets/Ori-animal-03.png';
+import oriAnimal4 from './assets/Ori-animal-04.png';
+import oriAnimal5 from './assets/Ori-animal-05.png';
+import oriAnimal6 from './assets/Ori-animal-06.png';
+import oriAnimal7 from './assets/Ori-animal-07.png';
+import oriStarl   from './assets/Ori-star-01.png';
+
 import jsxImg from './assets/jsx-ui.png';
 import stateImg from './assets/state-mgmt.png';
 
 export const CORE_CONCEPTS = [
   {
-    image: componentsImg,
-    title: 'Components',
+    image: oriFlower1,
+    title: 'Flower 4 petals',
     description:
-      'The core UI building block - compose the user interface by combining multiple components.',
+      'Nice flower. Easy level. It takes 15 minutes to create.',
   },
   {
-    image: jsxImg,
-    title: 'JSX',
+    image: oriFlower2,
+    title: 'Flower 6 petals',
     description:
-      'Return (potentially dynamic) HTML(ish) code to define the actual markup that will be rendered.',
+      'Original flower. Easy level. It takes 18 minutes to create.',
   },
   {
-    image: propsImg,
-    title: 'Props',
+    image: oriFlower3,
+    title: 'Flower 8 petals',
     description:
-      'Make components configurable (and therefore reusable) by passing input data to them.',
+      'More Anvance flower with more details. Medium level. It takes 22 minutes to create.',
   },
   {
-    image: stateImg,
-    title: 'State',
+    image: oriFlower4,
+    title: 'Flower 6 petals',
     description:
-      'React-managed data which, when changed, causes the component to re-render & the UI to update.',
+      'Another flower with unusual petals. Medium level. It takes 20 minutes to create.',
+  },
+  {
+    image: oriFlower5,
+    title: 'Flower 12 petals',
+    description:
+      'Simple simmertical flower. Easy level. It takes 18 minutes to create.',
+  },
+  {
+    image: oriAnimal1,
+    title: 'Elephant',
+    description:
+      'Animal\'s figure with a lot of foldings. Hard level. It takes 40 minutes to create.',
+  },
+  {
+    image: oriAnimal2,
+    title: 'Butterfly',
+    description:
+      'Butterfly\'s figure with a lot of foldings. Hard level. It takes 45 minutes to create.',
+  },
+  {
+    image: oriAnimal3,
+    title: 'Bird',
+    description:
+      'Bird\'s figure. Medium level. It takes 20 minutes to create.',
+  },
+  {
+    image: oriAnimal4,
+    title: 'Rino',
+    description:
+      'Animal\'s figure with a lot of foldings. Hard level. It takes 40 minutes to create.',
+  },
+  {
+    image: oriAnimal5,
+    title: 'Camel',
+    description:
+      'Animal\'s figure with a lot of foldings. Hard level. It takes 40 minutes to create.',
+  },
+  {
+    image: oriAnimal6,
+    title: 'Elepnant sitting',
+    description:
+      'Animal\'s figure with a lot of foldings. Hard level. It takes 40 minutes to create.',
+  },
+  {
+    image: oriAnimal7,
+    title: 'Bird 2',
+    description:
+      'Bird\'s figure. Medium level. It takes 20 minutes to create.',
+  },
+  {
+    image: oriStarl,
+    title: 'Star',
+    description:
+      'Geometrical figure similar to star. Easy level. It takes 10 minutes to create.',
   },
 ];
